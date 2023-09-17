6537d4d5b2613dab47b92ac84fd06fdb64dd780c
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2023-09-17 15:05:06 +0100

    edits

--- index.js
+++ index.js
@@ -1,56 +1,53 @@
 // let userInput = document.getElementById("date");
-let calcBtn = document.getElementById("calculalte");
+let calcBtn = document.getElementById("calculate");
 let result = document.getElementById("result");
 let userInput = document.querySelector(".date")
 
 userInput.max = new Date().toISOString().split("T")[0];
 
-function calculateAge(){
 
-let birthDate = new Date(userInput.value);
-console.log (userInput.value);
-console.log(birthDate);
+calcBtn.addEventListener("click", calculateAge)
 
-let currentDate = new Date();
+function calculateAge() {
 
-let birthDateDate = birthDate.getDate();
-let birthDateMonth = birthDate.getMonth()+1;
-let birthDateYear = birthDate.getFullYear();
+    let birthDate = new Date(userInput.value);
 
-let currentDateDate = currentDate.getDate();
-let currentDateMonth = currentDate.getMonth() + 1;
-let currentDateYear = currentDate.getFullYear();
-let ageYear, ageMonth, ageDay;
-ageYear = currentDateYear - birthDateYear
+    let currentDate = new Date();
 
-    if (currentDateMonth < birthDateMonth){
+    let birthDateDate = birthDate.getDate();
+    let birthDateMonth = birthDate.getMonth() + 1;
+    let birthDateYear = birthDate.getFullYear();
+
+    let currentDateDate = currentDate.getDate();
+    let currentDateMonth = currentDate.getMonth() + 1;
+    let currentDateYear = currentDate.getFullYear();
+    let ageYear, ageMonth, ageDay;
+    ageYear = currentDateYear - birthDateYear
+
+    if (currentDateMonth < birthDateMonth) {
         ageYear--;
         ageMonth = 12 + currentDateMonth - birthDateMonth;
-    }
-    else {
+    } else {
         ageMonth = currentDateMonth - birthDateMonth;
     }
 
-    if (currentDateDate <= birthDateDate){
+    if (currentDateDate <= birthDateDate) {
         ageMonth--;
         ageDay = getDaysInAMonth(birthDateYear, birthDateMonth) + currentDateDate - birthDateDate
-    }
-    else{
+    } else {
         ageDay = currentDateDate - birthDateDate;
     }
 
-    if (ageMonth<0){
+    if (ageMonth < 0) {
         ageMonth = 11;
         ageYear--;
     }
 
-    
-console.log(`You are ${ageYear} years, ${ageMonth} months and ${ageDay} days old.`); 
+    result.textContent = `You are ${ageYear} years, ${ageMonth} months and ${ageDay} days old.`
 
 }
 
-function getDaysInAMonth(year, month){
+function getDaysInAMonth(year, month) {
     return new Date(year, month, 0).getDate();
 }
 
-calcBtn.addEventListener("click", calculateAge);
